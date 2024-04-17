5462eeadd97bc275e4354b173a8e55d9398c5c62
noxito69 <ventasepg@hotmail.com>
2024-04-17 15:49:23 -0600

    register

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { errorInterceptor } from './interceptors/error.interceptor';
+import { tokenInterceptor } from './interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(withInterceptors([ tokenInterceptor, errorInterceptor]))]
 };
--- web/src/app/components/register/register.component.ts
+++ web/src/app/components/register/register.component.ts
@@ -1,24 +1,48 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RegisterService } from '../../services/register.service';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,HttpClientModule,ReactiveFormsModule,CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
-  username!: string;
-  email!: string;
-  password!: string;
+  registerForm = this.fb.group({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    passwordConfirm: new FormControl('', Validators.required),
+  });
 
-  onSubmit() {
-    console.log('Nombre de usuario: ', this.username);
-    console.log('Email: ', this.email);
-    console.log('Contraseña: ', this.password);
-    // Aquí puedes agregar la lógica para registrar al usuario
+  get username(){
+    return this.registerForm.get('username') as FormControl;
   }
+  get email(){
+    return this.registerForm.get('email') as FormControl;
+  }
+  get password(){
+    return this.registerForm.get('password') as FormControl;
+  }
+  get passwordConfirm(){
+    return this.registerForm.get('passwordConfirm') as FormControl;
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+
+
+    onSubmit():void {
+      console.warn(this.registerForm.value);
+  }
+
+
+
+  
 
 }
